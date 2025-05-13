8be44240a3acc1fdc7c14635ca4d54d2bc75142f
Pedro <pedro.rufino.martins@gmail.com>
2025-05-13 17:26:13 -0300

    new html

--- docs/ppk_test2.R
+++ docs/ppk_test2.R
@@ -7,20 +7,32 @@ moving_average <- function(series, window = 5) {
   stats::filter(series, rep(1 / window, window), sides = 2)
 }
 
+
 ppk <- read.csv("ppk-30minutes_sample_2emeio_trans5min.csv")
+ppk_sensor <- read.csv("ppk_sensor-only.csv")
+ppk_lora <- read.csv("ppk_just_send.csv")
 
 #summary(ppk)
-ppk$Timestamp.ms. <- round (ppk$Timestamp.ms. / (1000*60),  digits = 4)
-ppk$Current.uA. <- round (ppk$Current.uA. / 1000,  digits = 3)
+
+ppk$Timestamp.ms. <- round (ppk$Timestamp.ms.
+                            / (1000*60),  digits = 4)
+ppk_sensor$Timestamp.ms. <- round (ppk_sensor$Timestamp.ms.
+                                   / (1000*60),  digits = 4)
+ppk_lora$Timestamp.ms. <- round (ppk_lora$Timestamp.ms.
+                                 / (1000*60),  digits = 4)
+ppk$Current.uA. <- round (ppk$Current.uA.
+                          / 1000,  digits = 3)
+ppk_sensor$Current.uA. <- round (ppk_sensor$Current.uA.
+                                 / 1000,  digits = 3)
+ppk_lora$Current.uA. <- round (ppk_lora$Current.uA.
+                               / 1000,  digits = 3)
 
 names(ppk) [c(1,2)] <- c("time_m", "current_mA")
+names(ppk_sensor) [c(1,2)] <- c("time_m", "current_mA")
+names(ppk_lora) [c(1,2)] <- c("time_m", "current_mA")
 
-ppk$ma5 <- moving_average(ppk$current_mA, window = 5)
-ppk$ma10 <- moving_average(ppk$current_mA, window = 10)
-ppk$ma15 <- moving_average(ppk$current_mA, window = 15)
-ppk$ma20 <- moving_average(ppk$current_mA, window = 20)
 
-plot_ppk <- plot_ly(data = ppk, x = ~time_m) |>
+plot_ppk_full <- plot_ly(data = ppk, x = ~time_m) |>
   add_lines(y = ~current_mA, name = "Current (mA)",
             line = list(color = 'blue')) |>
   layout(
@@ -29,5 +41,21 @@ plot_ppk <- plot_ly(data = ppk, x = ~time_m) |>
     yaxis = list(title = "Current (mA)")
     )
 
+plot_ppk_sensor <- plot_ly(data = ppk_sensor, x = ~time_m) |>
+  add_lines(y = ~current_mA, name = "Current (mA)",
+            line = list(color = 'blue')) |>
+  layout(
+    title = "Current milliampere",
+    xaxis = list(title = "Time (minutes)"),
+    yaxis = list(title = "Current (mA)")
+  )
 
 
+plot_ppk_lora <- plot_ly(data = ppk_lora, x = ~time_m) |>
+  add_lines(y = ~current_mA, name = "Current (mA)",
+            line = list(color = 'blue')) |>
+  layout(
+    title = "Current milliampere",
+    xaxis = list(title = "Time (minutes)"),
+    yaxis = list(title = "Current (mA)")
+  )
